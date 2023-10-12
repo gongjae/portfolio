68181caa3e8b57c8ae5d4c695ea2e3d9964c3e07
gongjae <rudddong@gmail.com>
2023-10-13 00:16:52 +0900

    포트폴리오 2st

--- components/Header.jsx
+++ components/Header.jsx
@@ -17,55 +17,36 @@ const Header = () => {
               <div></div>
             </div>
             <div className="flex items-center font-bold">
-              {!userId ? (
-                //로그인이 안된 경우
-                <>
-                  <Link
-                    href="/sign-in"
-                    className="text-yellow-300 hover:text-white mr-4"
-                  >
-                    Sign In
-                  </Link>
-                  <Link
-                    href="/sign-up"
-                    className="text-yellow-300 hover:text-white mr-4"
-                  >
-                    Sign UP
-                  </Link>
-                </>
-              ) : (
-                //로그인된 경우
-                <>
-                  <Link
-                    href="/about"
-                    className="text-blue-300 hover:text-white mr-4"
-                  >
-                    About
-                  </Link>
-                  <Link
-                    href="/repos"
-                    className="text-blue-300 hover:text-white mr-4"
-                  >
-                    Repository
-                  </Link>
-                  <Link
-                    href="/teamproject"
-                    className="text-blue-300 hover:text-white mr-4"
-                  >
-                    Team Project
-                  </Link>
+              <>
+                <Link
+                  href="/about"
+                  className="text-blue-300 hover:text-white mr-4"
+                >
+                  About
+                </Link>
+                <Link
+                  href="/repos"
+                  className="text-blue-300 hover:text-white mr-4"
+                >
+                  Repository
+                </Link>
+                <Link
+                  href="/teamproject"
+                  className="text-blue-300 hover:text-white mr-4"
+                >
+                  Team Project
+                </Link>
 
-                  <Link
-                    href="/profile"
-                    className="text-blue-300 hover:text-white mr-4"
-                  >
-                    Profile
-                  </Link>
-                  <div className="ml-auto">
-                    <UserButton afterSignOutUrl="/" />
-                  </div>
-                </>
-              )}
+                <Link
+                  href="/profile"
+                  className="text-blue-300 hover:text-white mr-4"
+                >
+                  Profile
+                </Link>
+                <div className="ml-auto">
+                  <UserButton afterSignOutUrl="/" />
+                </div>
+              </>
             </div>
           </div>
         </div>
