7a47a54b46df1f2f881bd1cc987bc419e8f38e53
gongjae <rudddong@gmail.com>
2023-10-13 15:03:33 +0900

    포트폴리오 3st

--- components/Header.jsx
+++ components/Header.jsx
@@ -17,59 +17,36 @@ const Header = () => {
               <div></div>
             </div>
             <div className="flex items-center font-bold">
-              {!userId ? (
-                //로그인이 안된 경우
-                <>
-                  <div className="text-lg text-white p-7">
-                    {' '}
-                    로그인 해주세요~
-                  </div>
-                  <Link
-                    href="/sign-in"
-                    className="text-yellow-300 hover:text-white mr-4"
-                  >
-                    로그인
-                  </Link>
-                  <Link
-                    href="/sign-up"
-                    className="text-yellow-300 hover:text-white mr-4"
-                  >
-                    회원가입
-                  </Link>
-                </>
-              ) : (
-                //로그인된 경우
-                <>
-                  <Link
-                    href="/about"
-                    className="text-blue-300 hover:text-white mr-4"
-                  >
-                    About
-                  </Link>
-                  <Link
-                    href="/repos"
-                    className="text-blue-300 hover:text-white mr-4"
-                  >
-                    Repository
-                  </Link>
-                  <Link
-                    href="/teamproject"
-                    className="text-blue-300 hover:text-white mr-4"
-                  >
-                    Team Project
-                  </Link>
+              <>
+                <Link
+                  href="/about"
+                  className="text-blue-300 hover:text-white mr-4"
+                >
+                  About
+                </Link>
+                <Link
+                  href="/repos"
+                  className="text-blue-300 hover:text-white mr-4"
+                >
+                  Repository
+                </Link>
+                <Link
+                  href="/teamproject"
+                  className="text-blue-300 hover:text-white mr-4"
+                >
+                  Team Project
+                </Link>
 
-                  <Link
-                    href="/profile"
-                    className="text-blue-300 hover:text-white mr-4"
-                  >
-                    Profile
-                  </Link>
-                  <div className="ml-auto">
-                    <UserButton afterSignOutUrl="/" />
-                  </div>
-                </>
-              )}
+                <Link
+                  href="/profile"
+                  className="text-blue-300 hover:text-white mr-4"
+                >
+                  Profile
+                </Link>
+                <div className="ml-auto">
+                  <UserButton afterSignOutUrl="/" />
+                </div>
+              </>
             </div>
           </div>
         </div>
--- middleware.js
+++ middleware.js
@@ -1,6 +1,6 @@
 import { authMiddleware } from '@clerk/nextjs'
 export default authMiddleware({
-  publicRoutes: ['/', '/api/courses', '/repos'],
+  publicRoutes: ['/', '/api/courses', '/repos', '/about', '/teamproject'],
 })
 export const config = {
   matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
